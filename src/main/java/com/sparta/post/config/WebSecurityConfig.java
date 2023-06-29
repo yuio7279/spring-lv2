aa05d044800d3d26e15f16a184a36cfd93dcf0c8
yuio7279 <yuio7279@gmail.com>
2023-06-29 12:18:53 +0900

    인증 범위 설정을 위한 WebSecurityConfig 생성

--- src/main/java/com/sparta/post/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/post/config/WebSecurityConfig.java
@@ -62,13 +62,13 @@ public class WebSecurityConfig {
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
                         .requestMatchers("/api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
-                        .anyRequest().authenticated() // 그 외 모든 요청 인증처리
+                        .anyRequest().permitAll() // 그 외 모든 요청 인증처리
         );
 
-        http.formLogin((formLogin) ->
+/*        http.formLogin((formLogin) ->
                 formLogin
                         .loginPage("/api/user/login-page").permitAll()
-        );
+        );*/
 
         // 필터 관리
         http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
