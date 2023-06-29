66494a34786a491f5d89b9dd01237b10dbee5a6e
yuio7279 <yuio7279@gmail.com>
2023-06-29 20:36:42 +0900

    게시글 조회, 게시글 1개 조회 인증 허가처리

--- src/main/java/com/sparta/post/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/post/config/WebSecurityConfig.java
@@ -64,7 +64,7 @@ public class WebSecurityConfig {
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
                         .requestMatchers("api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
                         .requestMatchers("api/posts/{id}").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/posts").permitAll()
+                        .requestMatchers(HttpMethod.GET, "api/posts").permitAll()
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
